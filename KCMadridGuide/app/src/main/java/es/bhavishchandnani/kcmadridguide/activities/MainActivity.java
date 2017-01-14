3f0ff6921f590f9608f97b51810d104e4d468ee1
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-14 20:59:34 +0100

    Created Main and Shops Activities

---
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package es.bhavishchandnani.kcmadridguide.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import es.bhavishchandnani.kcmadridguide.R;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.activity_main_shops_btn)
+    private Button shopsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setupShopsBtn();
+
+    }
+
+    private void setupShopsBtn() {
+        shopsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activities/ShopsActivity.java
@@ -0,0 +1,17 @@
+package es.bhavishchandnani.kcmadridguide.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import es.bhavishchandnani.kcmadridguide.R;
+
+public class ShopsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops);
+
+
+    }
+}
