73449dcd63a3a2b9c246ec48b1e4bfcf8674263e
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-19 23:37:20 +0100

    Modified app to use interactors

--- KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/MadridGuideApp.java
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/MadridGuideApp.java
@@ -3,7 +3,13 @@ package es.bhavishchandnani.kcmadridguide;
 import android.app.Application;
 import android.content.Context;
 
+import com.squareup.picasso.Picasso;
+
 import java.lang.ref.WeakReference;
+
+import es.bhavishchandnani.kcmadridguide.interactor.CacheAllShopsInteractor;
+import es.bhavishchandnani.kcmadridguide.interactor.GetAllShopsInteractor;
+import es.bhavishchandnani.kcmadridguide.model.Shops;
 
 
 public class MadridGuideApp extends Application {
@@ -16,7 +22,30 @@ public class MadridGuideApp extends Application {
 
         // App Initialization
         appContext = new WeakReference(getApplicationContext());
+
+        //insertTestData();
+
+        new GetAllShopsInteractor().execute(getApplicationContext(),
+                new GetAllShopsInteractor.GetAllShopsInteractorResponse() {
+                    @Override
+                    public void response(Shops shops) {
+                        CacheShops(shops);
+                    }
+                });
+        Picasso.with(getApplicationContext()).setLoggingEnabled(true);
+        Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
     }
+
+    private void CacheShops(Shops shops) {
+        new CacheAllShopsInteractor().execute(getApplicationContext(), shops, new CacheAllShopsInteractor.CacheAllShopsInteractorResponse() {
+            @Override
+            public void response(boolean success) {
+                // Por ejemplo: Actualizar fecha del ultimo cacheo
+                // Ocultar el progressBar
+            }
+        });
+    }
+
 
     @Override
     public void onLowMemory() {
