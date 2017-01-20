43dfbdebc271f500558ce6702de4ecaf724d53cb
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-20 03:10:39 +0100

    Created Tests

--- KCMadridGuide/app/src/androidTest/java/es/bhavishchandnani/kcmadridguide/manager/db/provider/MadridGuideProviderTest.java
+++ KCMadridGuide/app/src/androidTest/java/es/bhavishchandnani/kcmadridguide/manager/db/provider/MadridGuideProviderTest.java
@@ -7,12 +7,15 @@ import android.net.Uri;
 import android.test.AndroidTestCase;
 
 import es.bhavishchandnani.kcmadridguide.manager.db.DBConstants;
+import es.bhavishchandnani.kcmadridguide.manager.db.MadridActivityDAO;
 import es.bhavishchandnani.kcmadridguide.manager.db.ShopDAO;
+import es.bhavishchandnani.kcmadridguide.model.MadridActivity;
 import es.bhavishchandnani.kcmadridguide.model.Shop;
 
 
 public class MadridGuideProviderTest extends AndroidTestCase{
 
+    //NOTE: Do not launch at the same time than DAO test they might be conflicts.
     public void testQueryAllShops() {
         ContentResolver cr = getContext().getContentResolver();
 
@@ -73,4 +76,65 @@ public class MadridGuideProviderTest extends AndroidTestCase{
         assertEquals(deletedShops, count);
     }
 
+
+
+    public void testQueryAllActivities() {
+        ContentResolver cr = getContext().getContentResolver();
+
+        Cursor c = cr.query(MadridGuideProvider.ACTIVITIES_URI, DBConstants.ALL_COLUMNS, null, null, null);
+
+        assertNotNull(c);
+    }
+
+
+    public void testInsertAnActivity() {
+        final ContentResolver cr = getContext().getContentResolver();
+
+        final MadridActivity activity = new MadridActivity(1, "Little activity of horrors!");
+        final Cursor beforeCursor = cr.query(MadridGuideProvider.ACTIVITIES_URI, DBConstants.ALL_COLUMNS, null, null, null);
+        final int beforeCount = beforeCursor.getCount();
+        final Uri insertedUri = cr.insert(MadridGuideProvider.ACTIVITIES_URI, MadridActivityDAO.getContentValues(activity));
+        assertNotNull(insertedUri);
+
+        final Cursor afterCursor = cr.query(MadridGuideProvider.ACTIVITIES_URI, DBConstants.ALL_COLUMNS, null, null, null);
+        final int afterCount = afterCursor.getCount();
+
+        assertEquals(afterCount, beforeCount +1);
+    }
+
+
+    public void testUpdateAnActivity() {
+        final ContentResolver cr = getContext().getContentResolver();
+
+        final MadridActivity activity = new MadridActivity(1, "Little activity of horrors!");
+        final Uri insertedUri = cr.insert(MadridGuideProvider.ACTIVITIES_URI, MadridActivityDAO.getContentValues(activity));
+        assertNotNull(insertedUri);
+        Cursor c = cr.query(insertedUri,DBConstants.ALL_COLUMNS, null, null, null);
+
+        activity.setName("Big activity of horrors!");
+        final int updateResult = cr.update(insertedUri, MadridActivityDAO.getContentValues(activity), null, null);
+        assertEquals(1, updateResult);
+    }
+
+    public void testDeleteAnActivity() {
+        final ContentResolver cr = getContext().getContentResolver();
+
+        final MadridActivity activity = new MadridActivity(1, "Little activity of horrors!");
+        final Uri insertedUri = cr.insert(MadridGuideProvider.ACTIVITIES_URI, MadridActivityDAO.getContentValues(activity));
+
+        final int deletedShops = cr.delete(insertedUri, null, null);
+
+        assertEquals(deletedShops, 1);
+    }
+
+    public void testDeleteAllActivities(){
+        ContentResolver cr = getContext().getContentResolver();
+
+        Cursor c = cr.query(MadridGuideProvider.ACTIVITIES_URI, DBConstants.ALL_COLUMNS, null, null, null);
+        int count = c.getCount();
+
+        final int deletedShops = cr.delete(MadridGuideProvider.ACTIVITIES_URI, null, null);
+
+        assertEquals(deletedShops, count);
+    }
 }
