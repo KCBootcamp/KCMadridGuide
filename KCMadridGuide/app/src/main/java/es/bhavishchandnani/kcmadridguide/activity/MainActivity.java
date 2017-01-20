22bbdf93a4470b6f360035f5afd221a2eb5740ea
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-20 08:07:48 +0100

    Modified Activities

--- KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activity/MainActivity.java
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activity/MainActivity.java
@@ -1,20 +1,35 @@
 package es.bhavishchandnani.kcmadridguide.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ViewSwitcher;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import es.bhavishchandnani.kcmadridguide.MadridGuideApp;
 import es.bhavishchandnani.kcmadridguide.R;
 import es.bhavishchandnani.kcmadridguide.navigator.Navigator;
+import es.bhavishchandnani.kcmadridguide.view.ActivityInterface;
+import es.bhavishchandnani.kcmadridguide.view.CustomViews;
+import es.bhavishchandnani.kcmadridguide.view.DialogListener;
 
-public class MainActivity extends AppCompatActivity {
+import static es.bhavishchandnani.kcmadridguide.MadridGuideApp.getAppContext;
+
+public class MainActivity extends AppCompatActivity{
     @BindView(R.id.activity_main_shops_btn)
     Button shopsBtn;
     @BindView(R.id.activity_main_activities_btn)
     Button activitiesBtn;
+    @BindView(R.id.activity_main_loading_view_switcher)
+    ViewSwitcher viewSwitcher;
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +37,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        ((MadridGuideApp)getAppContext()).setActivityInterface(getInterface());
+        ((MadridGuideApp)getAppContext()).init();
         setupShopsBtn();
         setupActivitiesBtn();
 
     }
+
+    @NonNull
+    private ActivityInterface getInterface() {
+        return new ActivityInterface() {
+
+            @Override
+            public void loadFinished(boolean success) {
+                hideProgressBar();
+                if (success) {
+                    shopsBtn.setEnabled(true);
+                    activitiesBtn.setEnabled(true);
+                }else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    dialog = CustomViews.buildGenericAlertDailog(builder, R.string.error_loading_dialog_title, R.string.error_loading_dialog_message, R.drawable.logo, android.R.string.ok, 0, new DialogListener() {
+                        @Override
+                        public void OnPositiveClick() {
+                            dialog.dismiss();
+                        }
+
+                        @Override
+                        public void OnNegativeClick() {
+
+                        }
+                    });
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void showMessage() {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                dialog = CustomViews.buildGenericAlertDailog(builder, R.string.check_conection_dialog_title, R.string.check_conection_dialog_message, R.drawable.logo, android.R.string.ok, 0, new DialogListener() {
+                    @Override
+                    public void OnPositiveClick() {
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void OnNegativeClick() {
+
+                    }
+                });
+                dialog.show();
+            }
+        };
+    }
+
 
     private void setupActivitiesBtn() {
         View.OnClickListener listener = new View.OnClickListener() {
@@ -46,4 +110,11 @@ public class MainActivity extends AppCompatActivity {
         };
         shopsBtn.setOnClickListener(listener);
     }
+
+    public void hideProgressBar(){
+        Animation fadeIn = AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in);
+        viewSwitcher.setAnimation(fadeIn);
+        viewSwitcher.showNext();
+    }
+
 }
--- KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activity/ShopsActivity.java
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/activity/ShopsActivity.java
@@ -1,11 +1,19 @@
 package es.bhavishchandnani.kcmadridguide.activity;
 
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import es.bhavishchandnani.kcmadridguide.R;
 import es.bhavishchandnani.kcmadridguide.fragment.ShopsFragment;
@@ -17,20 +25,19 @@ import es.bhavishchandnani.kcmadridguide.model.Shops;
 import es.bhavishchandnani.kcmadridguide.navigator.Navigator;
 import es.bhavishchandnani.kcmadridguide.view.OnElementClick;
 
-public class ShopsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class ShopsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private ShopsFragment shopsFragment;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
-
         shopsFragment = (ShopsFragment) getSupportFragmentManager().findFragmentById(R.id.activity_shops_fragment_shops);
         LoaderManager loaderManager = getSupportLoaderManager();
         loaderManager.initLoader(0, null, this);
     }
-
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -48,7 +55,7 @@ public class ShopsActivity extends AppCompatActivity implements LoaderManager.Lo
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         final Shops shops = ShopDAO.getShopsFromCursor(data);
         shopsFragment.setShops(shops);
-
+        initilizeMap(shops);
         shopsFragment.setOnElementClickListener(new OnElementClick<Shop>() {
             @Override
             public void elementClicked(Shop shop, int position) {
@@ -62,4 +69,37 @@ public class ShopsActivity extends AppCompatActivity implements LoaderManager.Lo
 
     }
 
+    private void initilizeMap(final Shops shops) {
+        if (googleMap == null) {
+            ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    ShopsActivity.this.googleMap = googleMap;
+                    setMarkers(shops);
+                }
+            });
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+        }
+
+        }
+
+    public void setMarkers(Shops shops) {
+        for (Shop shop: shops.allItems()) {
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(shop.getLatitude(),shop.getLongitude())).title(shop.getName());
+
+                //marker.icon(BitmapDescriptorFactory.fromPath(shop.getLogoImgUrl()));
+
+            googleMap.addMarker(marker);
+        }
+    }
 }
