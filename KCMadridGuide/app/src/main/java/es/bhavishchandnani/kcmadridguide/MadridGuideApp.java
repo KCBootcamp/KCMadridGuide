f92b3380ec9b38072c5c5b3a08396e346158f4ed
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-20 08:09:11 +0100

    Modifications in App to check cache validation

--- KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/MadridGuideApp.java
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/MadridGuideApp.java
@@ -2,10 +2,12 @@ package es.bhavishchandnani.kcmadridguide;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 import com.squareup.picasso.Picasso;
 
 import java.lang.ref.WeakReference;
+import java.util.Date;
 
 import es.bhavishchandnani.kcmadridguide.interactor.CacheAllActivitiesInteractor;
 import es.bhavishchandnani.kcmadridguide.interactor.CacheAllShopsInteractor;
@@ -13,11 +15,15 @@ import es.bhavishchandnani.kcmadridguide.interactor.GetAllActivitiesInteractor;
 import es.bhavishchandnani.kcmadridguide.interactor.GetAllShopsInteractor;
 import es.bhavishchandnani.kcmadridguide.model.MadridActivities;
 import es.bhavishchandnani.kcmadridguide.model.Shops;
+import es.bhavishchandnani.kcmadridguide.utils.CacheManager;
+import es.bhavishchandnani.kcmadridguide.utils.Connectivity;
+import es.bhavishchandnani.kcmadridguide.view.ActivityInterface;
 
 
 public class MadridGuideApp extends Application {
 
     private static WeakReference<Context> appContext;
+    private ActivityInterface listener;
 
     @Override
     public void onCreate() {
@@ -26,8 +32,29 @@ public class MadridGuideApp extends Application {
         // App Initialization
         appContext = new WeakReference(getApplicationContext());
 
-        //insertTestData();
+        Picasso.with(getApplicationContext()).setLoggingEnabled(true);
+        Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
+    }
 
+    public void init() {
+        boolean isConectionAvilable = Connectivity.isNetworkAvailable(getAppContext());
+        boolean cacheHasExpired = CacheManager.cacheHasExpired(getAppContext(),new Date());
+
+        if (cacheHasExpired) {
+            if (isConectionAvilable) {
+                callShopsInteractor();
+            } else  {
+
+                Log.d("TAG", "onCreate: No connection");
+                listener.showMessage();
+                listener.loadFinished(false);
+            }
+        }else{
+            listener.loadFinished(true);
+        }
+    }
+
+    private void callShopsInteractor() {
         new GetAllShopsInteractor().execute(getApplicationContext(),
                 new GetAllShopsInteractor.GetAllShopsInteractorResponse() {
                     @Override
@@ -35,6 +62,9 @@ public class MadridGuideApp extends Application {
                         CacheShops(shops);
                     }
                 });
+    }
+
+    private void callActivitiesInteractor() {
         new GetAllActivitiesInteractor().execute(getApplicationContext(),
                 new GetAllActivitiesInteractor.GetAllActivitiesInteractorResponse() {
                     @Override
@@ -42,27 +72,29 @@ public class MadridGuideApp extends Application {
                         CacheActivities(madridActivities);
                     }
                 });
-
-        Picasso.with(getApplicationContext()).setLoggingEnabled(true);
-        Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
     }
 
-    private void CacheActivities(MadridActivities madridActivities) {
-        new CacheAllActivitiesInteractor().execute(getApplicationContext(), madridActivities, new CacheAllActivitiesInteractor.CacheAllActivitiesInteractorResponse() {
-            @Override
-            public void response(boolean success) {
-                // Por ejemplo: Actualizar fecha del ultimo cacheo
-                // Ocultar el progressBar
-            }
-        });
-    }
 
     private void CacheShops(Shops shops) {
         new CacheAllShopsInteractor().execute(getApplicationContext(), shops, new CacheAllShopsInteractor.CacheAllShopsInteractorResponse() {
             @Override
             public void response(boolean success) {
-                // Por ejemplo: Actualizar fecha del ultimo cacheo
-                // Ocultar el progressBar
+                if(success){
+                    callActivitiesInteractor();
+                }
+            }
+        });
+    }
+
+
+    private void CacheActivities(MadridActivities madridActivities) {
+        new CacheAllActivitiesInteractor().execute(getApplicationContext(), madridActivities, new CacheAllActivitiesInteractor.CacheAllActivitiesInteractorResponse() {
+            @Override
+            public void response(boolean success) {
+                listener.loadFinished(success);
+                if (success){
+                    CacheManager.updateSyncDate(getAppContext(),new Date());
+                }
             }
         });
     }
@@ -76,5 +108,10 @@ public class MadridGuideApp extends Application {
 
     public static Context getAppContext() {
         return appContext.get();
+    }
+
+
+    public void setActivityInterface(ActivityInterface listener){
+        this.listener= listener;
     }
 }
