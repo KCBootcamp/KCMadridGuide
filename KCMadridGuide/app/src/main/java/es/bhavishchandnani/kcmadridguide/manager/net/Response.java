8732c0a8f112615b2dfccad66518738c80542467
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-20 01:26:59 +0100

    Modified Response Entity for Json Mapper

--- KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/manager/net/Response.java
+++ KCMadridGuide/app/src/main/java/es/bhavishchandnani/kcmadridguide/manager/net/Response.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class ShopResponse {
+public class Response<T> {
     @SerializedName("result")
-    List<ShopEntity> result;
+    List<T> result;
 }
